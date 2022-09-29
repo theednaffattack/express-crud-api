ab268ef00a9b412e103632d55d9ec090fd374e0e
Eddie Naff <eddie@eddienaff.dev>
2022-09-28 17:46:15 -0700

    Fix test

--- src/api/todos/todos.route.ts
+++ src/api/todos/todos.route.ts
@@ -1,13 +1,26 @@
 import Router, { Response } from "express";
-import { TodosCollection as Todos, TodoTypeWithId } from "./todos.model";
+import { asyncFn } from "../../utils/async-function";
+import { asyncWrap } from "../../utils/async-wrap";
+import {
+  TodosCollection as Todos,
+  TodoTypeWithId,
+  TodoType,
+} from "./todos.model";
 
 const router = Router();
 
-router.get("/", async (req, res: Response<TodoTypeWithId[]>) => {
+router.get("/", async (req, res: Response<TodoType[]>) => {
   const result = Todos.find();
-  const todos = await result.toArray();
-  console.log("VIEW TODOS", { todos });
-  res.json(todos);
+  const [todos, todosError] = await asyncWrap(() => result.toArray());
+  // Deal with any errors
+  if (todosError) {
+    console.error(todosError);
+  }
+  if (todos) {
+    res.json(todos);
+  } else {
+    console.error(todosError);
+  }
 });
 
 export { router };
--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -1,6 +1,31 @@
 import request from "supertest";
 
 import app from "../../app";
+import { asyncWrap } from "../../utils/async-wrap";
+import { TodosCollection } from "./todos.model";
+
+beforeAll(async () => {
+  // Check if we have any records in the database
+  const results = TodosCollection.find();
+  const [todos, todosError] = await asyncWrap(() => results.toArray());
+  if (todosError) {
+    console.error(todosError);
+  }
+
+  // If we find Todos in the database, drop the database
+  if (todos && todos.length) {
+    const [_collDrop, collDropError] = await asyncWrap(() =>
+      TodosCollection.drop()
+    );
+
+    // Report any errors we have dropping
+    if (collDropError) {
+      console.error("Error dropping collection for test", collDropError);
+    }
+    // Otherwise, success!
+    console.log("Success, your collection was dropped from the database");
+  }
+});
 
 describe("GET /api/v1/todos", () => {
   it("responds with an array of todos", async () => {
@@ -11,7 +36,7 @@ describe("GET /api/v1/todos", () => {
       .expect(200)
       .then((response) => {
         expect(response.body).toHaveProperty("length");
-        expect(response.body.length).toBe(1);
+        expect(response.body.length).toBe(0);
       });
   });
 });
