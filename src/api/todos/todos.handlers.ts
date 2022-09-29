6e18e29e8c929ee4b7a8f39b1091a30db307290d
Eddie Naff <eddie@eddienaff.dev>
2022-09-29 12:04:18 -0700

    Add error handling to 'handler' file

--- src/api/todos/todos.handlers.ts
+++ src/api/todos/todos.handlers.ts
@@ -1,18 +1,29 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import { asyncWrap } from "../../utils/async-wrap";
 import { TodoType, TodosCollection as Todos } from "./todos.model";
 
-export async function findAll(_req: Request, res: Response<TodoType[]>) {
+export async function findAll(
+  _req: Request,
+  res: Response<TodoType[]>,
+  next: NextFunction
+) {
   const result = Todos.find();
   const [todos, todosError] = await asyncWrap(() => result.toArray());
   // Deal with any errors
   if (todosError) {
     console.error(todosError);
+    next(todosError);
   }
   if (todos) {
     res.json(todos);
   } else {
-    console.error(todosError);
+    // If there are no todos and no todosError we have no idea what
+    // went wrong, so throw an error.
+    const error = {
+      message: "An unknown error connecting to database has occurred.",
+    };
+    console.error(error);
+    next(error);
   }
 }
