a5bce673cd575ce26b373151af611b820c925c8a
Eddie Naff <eddie@eddienaff.dev>
2022-09-28 17:45:14 -0700

    Add to jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
   modulePathIgnorePatterns: ["<rootDir>/dist/"],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-files-after-env.ts"],
+  testEnvironment: "node",
 };
