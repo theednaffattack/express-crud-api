8072a8ed2ed915088e21b975cb722cd96249af81
Eddie Naff <eddie@eddienaff.dev>
2022-09-29 16:19:33 -0700

    Add Todo creation handler with tests

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -1,20 +1,21 @@
 import request from "supertest";
 
 import app from "../../app";
-import { asyncWrap } from "../../utils/async-wrap";
+import { wrapAsync } from "../../utils/wrap-async";
+import { httpStatusCodes } from "../response-codes";
 import { TodosCollection } from "./todos.model";
 
 beforeAll(async () => {
   // Check if we have any records in the database
   const results = TodosCollection.find();
-  const [todos, todosError] = await asyncWrap(() => results.toArray());
+  const [todos, todosError] = await wrapAsync(() => results.toArray());
   if (todosError) {
     console.error(todosError);
   }
 
   // If we find Todos in the database, drop the database
   if (todos && todos.length) {
-    const [_collDrop, collDropError] = await asyncWrap(() =>
+    const [_collDrop, collDropError] = await wrapAsync(() =>
       TodosCollection.drop()
     );
 
@@ -37,6 +38,37 @@ describe("GET /api/v1/todos", () => {
       .then((response) => {
         expect(response.body).toHaveProperty("length");
         expect(response.body.length).toBe(0);
+      });
+  });
+});
+
+describe("POST /api/v1/todos", () => {
+  it("Responds with an error if the Todo is invalid.", async () => {
+    return request(app)
+      .post("/api/v1/todos")
+      .set("Accept", "application/json")
+      .send({ content: "" })
+      .expect("Content-Type", /json/)
+      .expect(httpStatusCodes[422].code)
+      .then((response) => {
+        expect(response.body).toHaveProperty("message");
+      });
+  });
+});
+
+describe("POST /api/v1/todos", () => {
+  it("Responds with an inserted object.", async () => {
+    return request(app)
+      .post("/api/v1/todos")
+      .set("Accept", "application/json")
+      .send({ content: "Learn TypeScript", done: false })
+      .expect("Content-Type", /json/)
+      .expect(httpStatusCodes[201].code)
+      .then((response) => {
+        expect(response.body).toHaveProperty("content");
+        expect(response.body).toHaveProperty("done");
+        expect(response.body).toHaveProperty("_id");
+        expect(response.body.content).toBe("Learn TypeScript");
       });
   });
 });
