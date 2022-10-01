4dc1ff93d1be9b705b7510f29215f98ae6cfadb0
Eddie Naff <eddie@eddienaff.dev>
2022-09-30 18:22:22 -0700

    Add finding a single Todo by ID

--- src/interfaces/params-with-id.ts
+++ src/interfaces/params-with-id.ts
@@ -1,5 +1,19 @@
+import { ObjectId } from "mongodb";
 import * as zod from "zod";
 
-export const ParamsWithId = zod.object({ id: zod.string().min(1) });
+export const ParamsWithIdSchema = zod.object({
+  id: zod
+    .string()
+    .min(1)
+    .refine((value) => {
+      try {
+        return new ObjectId(value);
+      } catch (err) {
+        // console.error("Unable to convert request params to Mongo ID.");
+        // console.error(err);
+        return false;
+      }
+    }),
+});
 
-export type ParamsWithIdType = zod.infer<typeof ParamsWithId>;
+export type ParamsWithIdType = zod.infer<typeof ParamsWithIdSchema>;
