8e7fdef4027c66ad14da3eacddf92882c2168e7a
Eddie Naff <eddie@eddienaff.dev>
2022-09-30 18:22:40 -0700

    Rename zod object for clarity

--- src/api/todos/todos.routes.ts
+++ src/api/todos/todos.routes.ts
@@ -1,5 +1,5 @@
 import Router from "express";
-import { ParamsWithId } from "../../interfaces/params-with-id";
+import { ParamsWithIdSchema } from "../../interfaces/params-with-id";
 import { validateRequest } from "../../middlewares";
 import * as TodosHandlers from "./todos.handlers";
 import { TodoSchema } from "./todos.model";
@@ -10,7 +10,7 @@ router.get("/", TodosHandlers.findAll);
 
 router.get(
   "/:id",
-  validateRequest({ params: ParamsWithId }),
+  validateRequest({ params: ParamsWithIdSchema }),
   TodosHandlers.findOne
 );
 
